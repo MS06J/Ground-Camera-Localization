c20e1d43ef9985f03cb904c94ea77159c7eca25e
Hsu, WenShuo <iamavery5417@gmail.com>
2024-08-26 16:47:57 +0200

    remove test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import time
-from pymavlink import mavutil
-
-connection = mavutil.mavlink_connection('udp:192.168.1.142:14550')
-print('waiting for heartbeat...')
-connection.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
